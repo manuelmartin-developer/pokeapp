61855be4393d5ebe9a68d5f92f1284e1bfdd72c8
Manuel Martin <manuelmad@homeria.com>
2021-09-30 20:24:21 +0200

    code: finish second functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
 import Footer from '../src/components/Footer';
 import Header from '../src/components/Header';
 import Main from '../src/components/Main';
+
 import '../src/styles/styles.scss';
 import './App.css';
 
+import { pokeContext } from './context/pokeContext';
+
+
 function App() {
+
+  const [pokemons, setPokemon] = useState([]);
+
+  const utils = {
+    pokemons: pokemons,
+    setPokemon: setPokemon
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
+      <pokeContext.Provider value={utils}>
         <Header />
         <Main />
+      </pokeContext.Provider>
       </BrowserRouter>
       <Footer />
     </div>
