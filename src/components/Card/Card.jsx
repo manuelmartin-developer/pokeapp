6f99340c6091778d973c3454f4201e5f71805cbb
Manuel Martin <manuelmad@homeria.com>
2021-09-30 17:58:28 +0200

    fix: minor style changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -27,10 +27,10 @@ const Card = (props) => {
           </article>
           <section className="card-info">
             <article className="card-stats">
-              <p className="card-stats-label"><span className="card-stats-data">stats:</span> {pokemon.stats ? pokemon.stats[0].base_stat : ""}</p>
-              <p className="card-stats-label"><span className="card-stats-data">type:</span> {pokemon.types ? pokemon.types[0].type.name : ""}</p>
-              <p className="card-stats-label"><span className="card-stats-data">weight:</span> {pokemon ? pokemon.weight : ""}</p>
-              <p className="card-stats-label"><span className="card-stats-data">height:</span> {pokemon ? pokemon.height : ""}</p>
+              <p className="card-stats-label"><span className="card-stats-data">stats</span> {pokemon.stats ? pokemon.stats[0].base_stat : ""}</p>
+              <p className="card-stats-label"><span className="card-stats-data">type</span> {pokemon.types ? pokemon.types[0].type.name : ""}</p>
+              <p className="card-stats-label"><span className="card-stats-data">weight</span> {pokemon ? pokemon.weight : ""}</p>
+              <p className="card-stats-label"><span className="card-stats-data">height</span> {pokemon ? pokemon.height : ""}</p>
             </article>
             <article className="card-image">
               {pokemon.sprites ? (
